d2d76ac1ad27dee45bb46a942e60e389cc618356
panzerstadt <tliqun@gmail.com>
2019-05-16 20:18:37 +0900

    Updates

---
+++ precache-manifest.b2c2263ebb26b0794a1bd90c878496d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea0b2664abb11cd57dc7a03aecb0918d",
+    "url": "https://panzerstadt.github.io/test-alicloud/index.html"
+  },
+  {
+    "revision": "985bb030203ba322d919",
+    "url": "https://panzerstadt.github.io/test-alicloud/static/css/main.880fc421.chunk.css"
+  },
+  {
+    "revision": "dfe370b050c4f5b80ccd",
+    "url": "https://panzerstadt.github.io/test-alicloud/static/js/2.6b514063.chunk.js"
+  },
+  {
+    "revision": "985bb030203ba322d919",
+    "url": "https://panzerstadt.github.io/test-alicloud/static/js/main.8f2af83f.chunk.js"
+  },
+  {
+    "revision": "8e157ed135f5e9ff5925",
+    "url": "https://panzerstadt.github.io/test-alicloud/static/js/runtime~main.18aa1514.js"
+  },
+  {
+    "revision": "798ce2689035bc7ed07c1f9bf75f754c",
+    "url": "https://panzerstadt.github.io/test-alicloud/static/media/SampleVideo_1280x720_10mb.798ce268.mp4"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "https://panzerstadt.github.io/test-alicloud/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/test-alicloud/precache-manifest.9732db05a09d1808ae68c6c6bb152d7b.js"
+  "https://panzerstadt.github.io/test-alicloud/precache-manifest.b2c2263ebb26b0794a1bd90c878496d8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/test-alicloud/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://panzerstadt.github.io/test-alicloud/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
