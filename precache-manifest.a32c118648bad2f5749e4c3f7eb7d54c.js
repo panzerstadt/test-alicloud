67d82b4f36237e9035ac855a65788736558d9ed1
panzerstadt <tliqun@gmail.com>
2019-05-16 20:07:49 +0900

    Updates

---
+++ precache-manifest.a32c118648bad2f5749e4c3f7eb7d54c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aac46ef7a8aeeacc9b2b4896a7b1721e",
+    "url": "/test-alicloud/index.html"
+  },
+  {
+    "revision": "41e8d752a937cb74de99",
+    "url": "/test-alicloud/static/css/main.880fc421.chunk.css"
+  },
+  {
+    "revision": "dfe370b050c4f5b80ccd",
+    "url": "/test-alicloud/static/js/2.6b514063.chunk.js"
+  },
+  {
+    "revision": "41e8d752a937cb74de99",
+    "url": "/test-alicloud/static/js/main.af4fd522.chunk.js"
+  },
+  {
+    "revision": "78199a9f874d657f5c25",
+    "url": "/test-alicloud/static/js/runtime~main.b23c979d.js"
+  },
+  {
+    "revision": "798ce2689035bc7ed07c1f9bf75f754c",
+    "url": "/test-alicloud/static/media/SampleVideo_1280x720_10mb.798ce268.mp4"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-alicloud/static/media/logo.5d5d9eef.svg"
+  }
+]);
